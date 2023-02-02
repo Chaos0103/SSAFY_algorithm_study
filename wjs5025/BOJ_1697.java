12312c724355a806b9658755f39d0b64107267c8
wjs_5025 <wjs_5025@naver.com>
2023-02-02 10:33:17 +0900

    숨바꼭질 / 90분 (resolve)

--- wjs5025/BOJ_1697.java
+++ wjs5025/BOJ_1697.java
@@ -1,3 +1,83 @@
+package BJ1697;
+
+import java.util.*;
+import java.io.*;
+
+/* Node 만들어서 여러 데이터를 가지게 해서
+해결하는 방법
+을 알려준 우택이형 감사합니댜 긋긋 
+ */
+class Node {
+	private int position;
+	private int count;
+
+	public Node(int position, int count) {
+		this.position = position;
+		this.count = count;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public int getCount() {
+		return count;
+	}
+}
+
+public class Main {
+	static int N, K;
+	static int max = 100001;
+	static boolean[] visited = new boolean[max];
+	static int minCnt = Integer.MAX_VALUE;
+
+	public static void bfs(int start) {
+		Queue<Node> q = new LinkedList<>();
+		q.offer(new Node(start, 0));
+		visited[start] = true;
+
+		while (!q.isEmpty()) {
+			Node poped = q.poll();
+
+			for (int i = 0; i < 3; i++) {
+				if (poped.getPosition() == K) {
+					if (minCnt > poped.getCount()) {
+						minCnt = poped.getCount();
+					}
+					return;
+				}
+
+				if (i == 0 && poped.getPosition() - 1 >= 0 && !visited[poped.getPosition() - 1]) {
+					q.offer(new Node(poped.getPosition() - 1, poped.getCount() + 1));
+					visited[poped.getPosition() - 1] = true;
+				}
+
+				if (i == 1 && poped.getPosition() + 1 < max && !visited[poped.getPosition() + 1]) {
+					q.offer(new Node(poped.getPosition() + 1, poped.getCount() + 1));
+					visited[poped.getPosition() + 1] = true;
+				}
+
+				if (i == 2 && poped.getPosition() * 2 < max && !visited[poped.getPosition() * 2]) {
+					q.offer(new Node(poped.getPosition() * 2, poped.getCount() + 1));
+					visited[poped.getPosition() * 1] = true;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		K = sc.nextInt();
+
+		bfs(N);
+
+		System.out.println(minCnt);
+	}
+}
+
+/*
 import java.util.*;
 import java.io.*;
 
@@ -5,7 +85,8 @@ import java.io.*;
  * 실패....
  * 주말에 보완하도록 하겠습니다.
  * 
- * */
+ */
+/*
 public class Main {
 	static int N, K;
 	static int max = 1000001;
@@ -51,3 +132,4 @@ public class Main {
 
 	}
 }
+ */
