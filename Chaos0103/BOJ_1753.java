b7782a4f196820e13d70fff0a5606a36138bace1
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2023-02-13 11:08:55 +0900

    최단경로 / 5분

---
+++ Chaos0103/BOJ_1753.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+class Node implements Comparable<Node> {
+    private int index;
+    private int distance;
+
+    public Node(int index, int distance) {
+        this.index = index;
+        this.distance = distance;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    @Override
+    public int compareTo(Node other) {
+        if (this.distance < other.distance) {
+            return -1;
+        }
+        return 1;
+    }
+}
+
+public class Main {
+
+    private static final int INF = Integer.MAX_VALUE;
+    private static int v, e, k;
+    private static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+    private static int[] d = new int[20001];
+
+    private static void dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
+        d[start] = 0;
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            int distance = node.getDistance();
+            int now = node.getIndex();
+            if (d[now] < distance) {
+                continue;
+            }
+            for (int i = 0; i < graph.get(now).size(); i++) {
+                int cost = d[now] + graph.get(now).get(i).getDistance();
+                if (cost < d[graph.get(now).get(i).getIndex()]) {
+                    d[graph.get(now).get(i).getIndex()] = cost;
+                    pq.offer(new Node(graph.get(now).get(i).getIndex(), cost));
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        v = sc.nextInt();
+        e = sc.nextInt();
+        k = sc.nextInt();
+
+        for (int i = 0; i <= v; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        Arrays.fill(d, INF);
+
+        for (int i = 0; i < e; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            graph.get(a).add(new Node(b, c));
+        }
+
+        dijkstra(k);
+
+        for (int i = 1; i <= v; i++) {
+            if (d[i] == INF) {
+                System.out.println("INF");
+            } else {
+                System.out.println(d[i]);
+            }
+        }
+    }
+}
